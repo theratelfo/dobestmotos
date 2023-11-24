78a34d060bd3cb500bacab178545834d084dc72d
davidtadeovargas <coritocorito@hotmail.com>
2023-11-24 00:20:09 -0600

    Delete target directory
    Directorio target no se sube

--- target/classes/respaldos/Categoria.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `categoria` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `codigo` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
